4ac3891b24f81b7bb33cc7d5e861f229349e1191
Miguel <42658991+MikeAnghelo@users.noreply.github.com>
2023-02-13 20:23:57 -0600

    creando carrito

---
+++ carrito.js
@@ -0,0 +1,13 @@
+const carrito = document.getElementById('carrito');
+const contenedorCarrito = document.querySelector('#lista-carrito tbody');
+const vaciarCarrito = document.getElementById('vaciar-carrito');
+const listaCursos = document.querySelector('#lista-cursos');
+
+let arregloCarrito = [];
+
+// Apartado de metodos
+
+function cargarEventos(){
+
+}
+
